3b7e80a21e01adde77ac2215e20bc54168910914
Arielle Jimera <arieru.dev@gmail.com>
2025-02-10 02:51:04 +0800

    feat: login page design

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
+
+void main() => runApp(MaterialApp(home: LoginScreen()
+    // routes: {
+    //   "/register": (context) => RegisterScreen(),
+    // },
+    ));

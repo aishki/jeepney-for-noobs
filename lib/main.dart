80c8f523585b71af164e021e998c2e3d1b609bc7
Arielle Jimera <arieru.dev@gmail.com>
2025-02-20 02:08:55 +0800

    fix: initialize route in home page instead of login

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
-// import 'screens/register_screen.dart';
+import 'package:jeepneyfornoobs_flutter/hidden_drawer.dart';
+// import 'screens/login_screen.dart';
+// import 'screens/home_screen.dart';
+// import 'package:hiddendrawertutorial/screens/home_screen.dart';
 
-void main() => runApp(MaterialApp(home: LoginScreen()
-    // routes: {
-    //   "/register": (context) => RegisterScreen(),
-    // },
-    ));
+void main() => runApp(JeepneyForNoobs());
+
+class JeepneyForNoobs extends StatelessWidget {
+  const JeepneyForNoobs({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HiddenDrawer(),
+      // routes: {
+      //   "/login": (context) => LoginScreen(),
+      // "/register": (context) => RegisterScreen(),
+      // },
+    );
+  }
+}
